6a7130c1cfb2b782122e374020e6d40c5bf842f0
Çağatay Atabey <atabeycagatay@gmail.com>
2020-12-22 01:39:25 +0300

    How To Login Firebird Database C#
    Using .Net Framework 4.7.2

---
+++ FirebirdLogin/FirebirdLogin/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace FirebirdLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(166, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(166, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.BackColor = System.Drawing.Color.SlateGray;
+            this.usernameTextBox.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.usernameTextBox.Location = new System.Drawing.Point(265, 25);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(147, 21);
+            this.usernameTextBox.TabIndex = 3;
+            this.usernameTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.usernameTextBox_KeyPress);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.BackColor = System.Drawing.Color.SlateGray;
+            this.passwordTextBox.Location = new System.Drawing.Point(265, 64);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.passwordTextBox.MaxLength = 20;
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(147, 21);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // loginButton
+            // 
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.loginButton.Image = ((System.Drawing.Image)(resources.GetObject("loginButton.Image")));
+            this.loginButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.loginButton.Location = new System.Drawing.Point(265, 104);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(147, 27);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(9, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Made By Cagatay Atabey-2020";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(238, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 18);
+            this.label4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(441, 159);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Firebird Login Page";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FirebirdLogin/FirebirdLogin/Form1.cs
@@ -0,0 +1,88 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace FirebirdLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //*******************************IF YOU LOGIN WITH DATABASE CONVERT THE COMMENT CODE TO NORMAL CODE ***************************//
+
+
+        //FbConnection fbCon = new FbConnection(ConfigurationManager.ConnectionStrings["FB"].ConnectionString);
+        //FbDataReader dr;
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            //-------------------------------------BASE DB USERNAME AND PASSWORD-----------------------------------//
+
+            //FbCommand cmd = new FbCommand();
+            //cmd.CommandText = "SELECT * FROM KULLANIC WHERE USER_NAME='"+usernameTextBox.Text+"' AND SIFRE='"+passwordTextBox.Text+"' "; //SQL Text
+            //cmd.Connection = fbCon; //Command Connection Db
+            //fbCon.Open();
+            //dr = cmd.ExecuteReader();
+            //if (dr.Read())
+            //{
+            //    MessageBox.Show("Ok. Login Successful");
+            //    label4.ForeColor = Color.Green;
+            //    fbCon.Close();
+            //    Process.Start(@"C:\Netsim.N4\Bin\NetsimApp.exe");
+            //}
+            //else
+            //{
+            //    fbCon.Close();
+            //    label4.Text="Username and Password is Invalid";
+            //    label4.ForeColor = Color.Red;
+            //}
+            //fbCon.Close();
+
+
+            //----------------------------------STATIC USERNAME AND PASSWORD-----------------------------//
+            if (string.IsNullOrEmpty(usernameTextBox.Text) || string.IsNullOrEmpty(passwordTextBox.Text))
+            {
+                label4.Text = "You Must be input any character ";
+                label4.ForeColor = Color.Red;
+
+            }
+            else
+            {
+                if (usernameTextBox.Text == "SYSDBA" && passwordTextBox.Text == "admin")
+                {
+                    label4.Text = "Ok. Login Successful";
+                    label4.ForeColor = Color.Green;
+                    Process.Start(@"C:\Netsim.N4\Bin\NetsimApp.exe");  //Open The App
+                }
+                else
+                {
+
+                    label4.Text = "Username and Password is Invalid";
+                    label4.ForeColor = Color.Red;  //if get error Label Color Red
+                    usernameTextBox.Clear();       //if get error TextBox Auto Clear
+                    passwordTextBox.Clear();       //if get error TextBox Auto Clear
+                }
+            }
+        }
+
+        //Convert Username Upper Case
+        private void usernameTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            e.KeyChar = (e.KeyChar.ToString()).ToUpper().ToCharArray()[0];
+
+        }
+    }
+}
